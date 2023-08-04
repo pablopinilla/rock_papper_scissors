deb5f59ccd1cab805fdce2181a1e8cdd770df2b9
Pablo Pinilla <140816109+pablopinilla@users.noreply.github.com>
2023-08-04 09:21:23 -0500

    Added the playRps function which confronts a player slection with the machine selection and determins a winner based on the normal Rock, Paper, Scissors game's criteria. Started to implement function Game which will play five rounds of RPS and keep score for both player and machine

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-console.log("Supp!")
 // Get a random choice to act as the computer play. => Make a function that returns a random selection between Rock, Paper or Scissors.
 
 let choice = "";
-let rand = 0
 
 function random(){
     return Math.floor(Math.random() * 3) + 1;
@@ -10,13 +8,55 @@ function random(){
 
 function getComputerChoice (num){
     if (num === 1){
-        choice = "Rock"
+        choice = "Rocks"
     } else if (num === 2){
-        choice = "Papper"
+        choice = "Paper"
     } else {
         choice = "Scissors"
     }
-    console.log(choice)
+    return choice
 }
 
-getComputerChoice(random())
+
+
+// Play a round of R,P,S. between the user and the machine
+
+function playRps(userChoice, computerChoice = "Rocks"){
+    if (userChoice === "Rocks" && computerChoice === "Rocks"){
+        return("Ahh!! Seems like a tie. Try again.")
+    } else if (userChoice === "Rocks" && computerChoice === "Scissors"){
+        return("You lose! Scissors beat Rocks.")
+    } else if (userChoice === "Rocks" && computerChoice === "Paper"){
+        return("You win! Rocks beat Paper.")
+    } else if (userChoice === "Paper" && computerChoice === "Paper"){
+        return("Ahh!! Seems like a tie. Try again.")
+    } else if (userChoice === "Paper" && computerChoice === "Scissors"){
+        return("You lose! Scissors beat Paper.")
+    } else if (userChoice === "Paper" && computerChoice === "Rocks"){
+        return("You win! Paper beats Rocks.")
+    } else if (userChoice === "Scissors" && computerChoice === "Scissors"){
+        return("Ahh!! Seems like a tie. Try again.")
+    } else if (userChoice === "Scissors" && computerChoice === "Rocks"){
+        return("You lose! Rocks beat Scissors.")
+    } else if (userChoice === "Scissors" && computerChoice === "Paper"){
+        return("You win! Scissors beat Paper.")
+    }
+}
+
+
+
+//
+
+//Play five rounds and for each round keep the score.
+
+function game(){
+    for(let i = 0; i < 5; i++){
+
+        const playerSelection = "Scissors"
+        const computerSelection = getComputerChoice(random());
+        console.log(playRps(playerSelection, computerSelection))
+
+    }
+}
+
+game()
