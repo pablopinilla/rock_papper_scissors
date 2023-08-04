0a1a8228fa7724b1754aeef842e56b64347c4372
Pablo Pinilla <140816109+pablopinilla@users.noreply.github.com>
2023-08-04 08:06:24 -0500

    Created a function named random that returns a random number between 1 and 3 to pass as an argument to a funcion named getComputerChoice that selects a play based on the number given (3 options available

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log("SUpp!")
+console.log("Supp!")
+// Get a random choice to act as the computer play. => Make a function that returns a random selection between Rock, Paper or Scissors.
+
+let choice = "";
+let rand = 0
+
+function random(){
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+function getComputerChoice (num){
+    if (num === 1){
+        choice = "Rock"
+    } else if (num === 2){
+        choice = "Papper"
+    } else {
+        choice = "Scissors"
+    }
+    console.log(choice)
+}
+
+getComputerChoice(random())
